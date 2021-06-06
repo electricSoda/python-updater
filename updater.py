a856916d62d542abd0c5e8209429801d9e8821a8
electricSoda <justin.hua.ge@gmail.com>
2021-06-06 09:55:38 -0600

    initial commit

---
+++ updater.py
@@ -0,0 +1,20 @@
+print("Hi")
+
+import requests
+import re
+
+class Update:
+    def __init__(self, target_link):
+        self.target_link = target_link
+
+        r = requests.get(self.target_link, allow_redirects=True)
+        filename = self.getFileName(self.target_link)
+        open(filename, "wb").write(r.content)
+
+    def getFileName(self, url):
+        fname = url.split("/")
+        fname = fname[len(fname)-1]
+        return fname
+
+
+Update("")
