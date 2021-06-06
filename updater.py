44da6e2e7ec7f57f5bd0d655e831c9aee33c5c2b
electricSoda <justin.hua.ge@gmail.com>
2021-06-06 10:09:31 -0600

    test updater.py

--- updater.py
+++ updater.py
@@ -2,10 +2,19 @@ print("Hi")
 
 import requests
 import re
+import os
 
 class Updater:
     def __init__(self, target_link):
         self.target_link = target_link
+    def checkUpdate(self, link):
+        os.chdir("C:\\teleport\\Code\\Python Updating Concept\\")
+        r = requests.get(link, allow_redirects=True)
+        filename = self.getFileName(link)
+
+
+    def update(self):
+        os.chdir("C:\\teleport\\Code\\Python Updating Concept\\updated")
 
         r = requests.get(self.target_link, allow_redirects=True)
         filename = self.getFileName(self.target_link)
