26ec017691f37dc074f2ce4ddc05ae336e33ffeb
electricSoda <justin.hua.ge@gmail.com>
2021-06-06 10:18:35 -0600

    added a local thingy

--- updater.py
+++ updater.py
@@ -7,11 +7,21 @@ import os
 class Updater:
     def __init__(self, target_link):
         self.target_link = target_link
+
     def checkUpdate(self, link):
         os.chdir("C:\\teleport\\Code\\Python Updating Concept\\")
         r = requests.get(link, allow_redirects=True)
         filename = self.getFileName(link)
+        open(filename, "wb").write(r.content)
+        f = open(filename, "r")
+        f1 = open("updatebranchLOCAL.txt", "r")
 
+        if str(f.readline()) == str(f1.readline()):
+            print(f.readline(), "Up to date")
+            return False
+        else:
+            print(f.readline(), "Different than (local): " + f1.readline())
+            return True
 
     def update(self):
         os.chdir("C:\\teleport\\Code\\Python Updating Concept\\updated")
@@ -26,4 +36,5 @@ class Updater:
         return fname
 
 
-Updater("https://github.com/electricSoda/python-updater/blob/main/updater.py")
+u = Updater("https://github.com/electricSoda/python-updater/blob/main/updater.py")
+u.checkUpdate("https://raw.githubusercontent.com/electricSoda/python-updater/main/upatebranch.txt")
